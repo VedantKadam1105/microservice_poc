6056c8587e0260892fe43e4dce096888446a8013
Vedant Kadam <vedantkd23@gmail.com>
2025-07-30 20:57:39 +0530

    Rating service working fine

---
+++ RatingService/src/main/java/com/rating/RatingService/controllers/RatingController.java
@@ -0,0 +1,35 @@
+package com.rating.RatingService.controllers;
+
+import com.rating.RatingService.entities.Rating;
+import com.rating.RatingService.services.RatingService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/ratings")
+public class RatingController {
+    @Autowired
+    private RatingService ratingService;
+    @PostMapping
+    public ResponseEntity<Rating> createRating(@RequestBody Rating rating){
+        return ResponseEntity.status(HttpStatus.CREATED).body(ratingService.createRating(rating));
+    }
+    @GetMapping
+    public ResponseEntity<List<Rating>> getAllRating(){
+        return ResponseEntity.status(HttpStatus.OK).body(ratingService.getRatings());
+    }
+
+    @GetMapping("/user/{userid}")
+    public ResponseEntity<List<Rating>> getAllRatingByUserId(@PathVariable String userid){
+        return ResponseEntity.status(HttpStatus.OK).body(ratingService.getRatingByUserId(userid));
+    }
+
+    @GetMapping("/hotel/{hotelid}")
+    public ResponseEntity<List<Rating>> getAllRatingByHotelId(@PathVariable String hotelid){
+        return ResponseEntity.status(HttpStatus.OK).body(ratingService.getRatingByHotelId(hotelid));
+    }
+}
