7b9906b7b6635bca2c24e3cf5ed04fedd64cf35e
Vedant Kadam <vedantkd23@gmail.com>
2025-07-31 15:57:16 +0530

    requestmapping changed

--- HotelService/src/main/java/com/hotelservice/HotelService/controllers/HotelController.java
+++ HotelService/src/main/java/com/hotelservice/HotelService/controllers/HotelController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/v1/hotels")
+@RequestMapping("/hotels")
 public class HotelController {
     @Autowired
     private HotelService hotelService;
--- RatingService/src/main/java/com/rating/RatingService/controllers/RatingController.java
+++ RatingService/src/main/java/com/rating/RatingService/controllers/RatingController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/ratings")
+@RequestMapping("/ratings")
 public class RatingController {
     @Autowired
     private RatingService ratingService;
